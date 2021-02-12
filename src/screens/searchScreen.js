49a19e5f79b60741267a51cbf6caf0de287ff8d2
ahmetizgi <dev.ahmetizgi@gmail.com>
2021-02-12 12:28:03 +0300

    useResults custom hook added

--- src/screens/searchScreen.js
+++ src/screens/searchScreen.js
@@ -1,35 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const searchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term,
-          location: "istanbul",
-        },
-      });
-      setResults(response.data.businesses);
-      setErrorMessage("");
-    } catch (err) {
-      setErrorMessage("Something went wrong");
-    }
-  };
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View style={styles.container}>
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text> {errorMessage} </Text> : null}
       <Text>We have found {results.length} results </Text>
