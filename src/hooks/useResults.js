9b2d4194c7e5f298b3f488d2311de079d87f6e6a
ahmetizgi <dev.ahmetizgi@gmail.com>
2021-02-12 13:58:09 +0300

    ResultList component added

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -22,7 +22,7 @@ export default () => {
   };
 
   useEffect(() => {
-    searchApi("kebap");
+    searchApi("");
   }, []);
 
   return [searchApi, results, errorMessage];
--- src/screens/searchScreen.js
+++ src/screens/searchScreen.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import ResultsList from "../components/ResultsList";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
 
 const searchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter((item) => {
+      return item.price === price;
+    });
+  };
 
   return (
     <View style={styles.container}>
@@ -16,6 +24,9 @@ const searchScreen = () => {
       />
       {errorMessage ? <Text> {errorMessage} </Text> : null}
       <Text>We have found {results.length} results </Text>
+      <ResultsList results={filterResultsByPrice("₺")} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice("₺₺")} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice("₺₺₺")} title="Big Spender" />
     </View>
   );
 };
